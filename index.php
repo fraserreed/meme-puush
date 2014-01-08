5a4c1e9dde5b145dacfa64cc799c64ed674b36a3
Fraser Reed <fraser@canvaspop.com>
2014-01-08 00:11:00 -0500

    added test form, some more text tweaks

--- index.php
+++ index.php
@@ -1,129 +1,187 @@
 <?php
 
-//set the output type
-$output = 'file';
-
-if( isset( $output ) && $output == 'file' )
+try
 {
+
+    //output type
+    $output = strtolower( ( isset( $_GET[ 'output' ] ) ) ? $_GET[ 'output' ] : 'file' );
+
     //set api key
     $apiKey = strtoupper( ( isset( $_GET[ 'apiKey' ] ) ) ? $_GET[ 'apiKey' ] : '' );
 
-    if( !$apiKey )
+    //caption to write on the image
+    $caption = strtoupper( ( isset( $_GET[ 'caption' ] ) ) ? $_GET[ 'caption' ] : '' );
+
+    //location - top or bottom
+    $location = ( isset( $_GET[ 'location' ] ) ) ? $_GET[ 'location' ] : 'top';
+
+    //source image to overlay
+    $img = ( isset( $_GET[ 'img' ] ) ) ? $_GET[ 'img' ] : '';
+
+    if( $output == 'file' && !$apiKey )
         throw new \Exception( 'Puu.sh api key must be provided' );
+
+    if( !$caption )
+        throw new \Exception( 'No caption provided' );
+
+    if( !$img )
+        throw new \Exception( 'No image url provided' );
+
+    //get the source width and height of the image url
+    list( $w, $h ) = getimagesize( $img );
+
+    //if the image doesn't exist, cannot continue
+    if( $w == 0 || $h == 0 )
+        throw new \Exception( 'Could not load image: ' . $img );
+
+    //create the base image
+    $image = new Imagick( $img );
+
+    $draw = new ImagickDraw();
+    //set the font
+    $draw->setFont( './impact.ttf' );
+    $draw->setFontSize( 72 );
+    //set stroke colour to black
+    $draw->setStrokeColor( new ImagickPixel( "#000000" ) );
+    $draw->setStrokeAntialias( true );
+    $draw->setTextAntialias( true );
+    //north gravity is top center
+    $draw->setGravity( Imagick::GRAVITY_NORTH );
+
+    //set alignment to center
+    $draw->setTextAlignment( 2 );
+
+    //get the target font size
+    list( $fontSize, $caption, $textProperties ) = getFontSize( $image, $draw, $caption );
+
+    $draw->setFontSize( $fontSize );
+    $captionRows = substr_count( $caption, "\n" ) + 1;
+
+    //set stroke based on font size
+    $strokeWidth = (int) min( array( 5, ceil( $fontSize / 12 ) ) );
+    $draw->setStrokeWidth( $strokeWidth );
+
+    //set the new font size
+    $draw->setFontSize( $fontSize );
+
+    //set font colour to black initially to create a smooth stroke
+    $draw->setFillColor( new ImagickPixel( "#000000" ) );
+    //and make the stroke transparent
+    $draw->setStrokeAlpha( 100 );
+
+    //center the text horizontally
+    $xPos = $w / 2;
+
+    //determine the vertical position based on the top / bottom location
+    switch( $location )
+    {
+        case 'top':
+            $yPos = $fontSize + 10;
+            break;
+        case 'bottom':
+        default:
+            if( $captionRows == 1 )
+                $yPos = $h - $textProperties[ 'boundingBox' ][ 'y2' ];
+            else
+                $yPos = $h - $textProperties[ 'textHeight' ];
+            break;
+    }
+
+    //write the text on the image
+    $image->annotateImage( $draw, $xPos, $yPos, 0, $caption );
+
+    //now change the fill colour to black
+    $draw->setFillColor( new ImagickPixel( '#FFFFFF' ) );
+    //and make the stroke transparent
+    $draw->setStrokeAlpha( 0 );
+
+    //re-write the text on the image
+    $image->annotateImage( $draw, $xPos, $yPos, 0, $caption );
+    $image->setImageFormat( "jpg" );
+    $image->setCompression( Imagick::COMPRESSION_JPEG );
+    $image->setCompressionQuality( 70 );
+
+    if( isset( $output ) && $output == 'file' )
+    {
+        $filename = '/tmp/' . microtime( true ) . '.jpg';
+
+        //finally output the image
+        $image->writeimage( $filename );
+
+        echo json_encode( array( 'url' => uploadImage( $filename, $apiKey ) ) );
+
+        //remove the file
+        unlink( $filename );
+    }
+    else
+    {
+        header( 'Content-type: image/jpg' );
+        echo $image;
+    }
 }
-
-//caption to write on the image
-$caption = strtoupper( ( isset( $_GET[ 'caption' ] ) ) ? $_GET[ 'caption' ] : '' );
-
-//location - top or bottom
-$location = ( isset( $_GET[ 'location' ] ) ) ? $_GET[ 'location' ] : 'top';
-
-//source image to overlay
-$img = ( isset( $_GET[ 'img' ] ) ) ? $_GET[ 'img' ] : '';
-
-if( !$img )
-    throw new \Exception( 'No image url provided' );
-
-//get the source width and height of the image url
-list( $w, $h ) = getimagesize( $img );
-
-//if the image doesn't exist, cannot continue
-if( $w == 0 || $h == 0 )
-    throw new \Exception( 'Could not load image: ' . $img );
-
-//create the base image
-$image = new Imagick( $img );
-
-$draw = new ImagickDraw();
-//set the font
-$draw->setFont( './impact.ttf' );
-$draw->setFontSize( 72 );
-//set stroke colour to black
-$draw->setStrokeColor( new ImagickPixel( "#000000" ) );
-$draw->setStrokeAntialias( true );
-$draw->setTextAntialias( true );
-//north gravity is top center
-$draw->setGravity( Imagick::GRAVITY_NORTH );
-
-//set alignment to center
-$draw->setTextAlignment( 2 );
-
-$fontSize = 0;
-$rows     = 1;
-
-$imgSize          = $image->getImageGeometry();
-$textDesiredWidth = intval( $imgSize[ 'width' ] * .90 );
-
-list( $fontSize, $caption ) = getFontSize( $image, $draw, $caption );
-
-$draw->setFontSize( $fontSize );
-$captionRows = substr_count( $caption, "\n" ) + 1;
-
-//set stroke based on font size
-$strokeWidth = (int) min( array( 2, ceil( $fontSize / 12 ) ) );
-$draw->setStrokeWidth( $strokeWidth );
-
-//set the new font size
-$draw->setFontSize( $fontSize );
-
-//set font colour to black initially to create a smooth stroke
-$draw->setFillColor( new ImagickPixel( "#000000" ) );
-//and make the stroke transparent
-$draw->setStrokeAlpha( 100 );
-
-//center the text horizontally
-$xPos = $w / 2;
-
-//determine the vertical position based on the top / bottom location
-switch( $location )
+catch( \Exception $e )
 {
-    case 'top':
-        $yPos = $fontSize + 10;
-        break;
-    case 'bottom':
-    default:
-        $yPos = $h - ( $fontSize * $captionRows ) - 10;
-        break;
-}
+    ?>
+<html>
+<head>
+    <title>Meme Generator</title>
+    <style>
+        .text input {
+            width: 400px;
+        }
+    </style>
+</head>
+<body>
 
-//write the text on the image
-$image->annotateImage( $draw, $xPos, $yPos, 0, $caption );
+<form action="index.php">
+    <table>
+        <tr>
+            <td><label for="output">Output Type:</label></td>
+            <td>
+                <input id="output" type="radio" name="output" value="file" <?php echo ( $output == 'file' ) ? 'checked' : ''?>/> File<br/>
+                <input type="radio" name="output" value="screen" <?php echo ( $output != 'file' ) ? 'checked' : ''?>/> Screen
+            </td>
+        </tr>
+        <tr class="text">
+            <td><label for="apiKey">puu.sh Api Key:</label></td>
+            <td><input id="apiKey" type="text" name="apiKey" value="<?php echo isset( $apiKey ) ? $apiKey : ''; ?>"/></td>
+        </tr>
+        <tr class="text">
+            <td><label for="img">Url:</label></td>
+            <td><input id="img" type="text" name="img" value="<?php echo isset( $img ) ? $img : ''; ?>"/></td>
+        </tr>
+        <tr class="text">
+            <td><label for="caption">Caption:</label></td>
+            <td><input id="caption" type="text" name="caption" value="<?php echo isset( $caption ) ? $caption : ''; ?>"/></td>
+        </tr>
+        <tr>
+            <td><label for="location">Location:</label></td>
+            <td>
+                <input id="location" type="radio" name="location" value="top" <?php echo ( $location == 'top' ) ? 'checked' : ''?>/> Top<br/>
+                <input type="radio" name="location" value="bottom" <?php echo ( $location != 'top' ) ? 'checked' : ''?>/> Bottom
+            </td>
+        </tr>
 
-//now change the fill colour to black
-$draw->setFillColor( new ImagickPixel( '#FFFFFF' ) );
-//and make the stroke transparent
-$draw->setStrokeAlpha( 0 );
+    </table>
+    <button type="submit">Create Meme</button>
+</form>
 
-//re-write the text on the image
-$image->annotateImage( $draw, $xPos, $yPos, 0, $caption );
-$image->setImageFormat( "jpg" );
-$image->setCompression( Imagick::COMPRESSION_JPEG );
-$image->setCompressionQuality( 70 );
+Error: <?php echo $e->getMessage(); ?>
 
-if( isset( $output ) && $output == 'file' )
-{
-    $filename = '/tmp/' . microtime( true ) . '.jpg';
+</body>
+</html>
 
-    //finally output the image
-    $image->writeimage( $filename );
 
-    echo json_encode( array( 'url' => uploadImage( $filename, $apiKey ) ) );
-
-    //remove the file
-    unlink( $filename );
-}
-else
-{
-    header( 'Content-type: image/jpg' );
-    echo $image;
+<?php
 }
 
 /**
  * @param Imagick     $image
  * @param ImagickDraw $draw
  * @param             $caption
+ * @param int         $rows
  *
- * @return int
+ * @return array
  */
 function getFontSize( Imagick $image, ImagickDraw $draw, $caption, $rows = 1 )
 {
@@ -131,12 +189,13 @@ function getFontSize( Imagick $image, ImagickDraw $draw, $caption, $rows = 1 )
     $textProperties = array( 'textWidth' => 0 );
 
     //make sure text is no wider than 94% of image size
-    $imgSize          = $image->getImageGeometry();
-    $textDesiredWidth = intval( $imgSize[ 'width' ] * .88 );
+    $imgSize             = $image->getImageGeometry();
+    $textDesiredWidth    = intval( $imgSize[ 'width' ] * .88 );
+    $minTextDesiredWidth = intval( $imgSize[ 'width' ] * .60 );
 
     //set the max and min font sizes based on the height and string length
-    $maxFont = floor( $imgSize[ 'height' ] * .065 );
-    $minFont = floor( $imgSize[ 'height' ] * .045 ) * ( min( 1, ( $imgSize[ 'height' ] * .33 ) / strlen( $caption ) ) );
+    $maxFont = floor( $imgSize[ 'height' ] * .065 ) * ( min( 1, ( $imgSize[ 'height' ] * .33 ) / strlen( $caption ) ) );
+    $minFont = floor( $imgSize[ 'height' ] * .055 ) * ( min( 1, ( $imgSize[ 'height' ] * .33 ) / strlen( $caption ) ) );
 
     // Set an initial value for the fontsize, will be increased in the loop below
     $fontSize = 0;
@@ -145,10 +204,16 @@ function getFontSize( Imagick $image, ImagickDraw $draw, $caption, $rows = 1 )
     while( $textProperties[ 'textWidth' ] <= $textDesiredWidth )
     {
         $draw->setFontSize( $fontSize );
+        $draw->setTextKerning( min( 4, $fontSize / 24 ) );
         $textProperties = $image->queryFontMetrics( $draw, $caption );
         $fontSize++;
 
-        if( $fontSize >= $maxFont )
+        //set a threshold so the font doesn't get too big for short strings
+        if( $fontSize >= ( $maxFont * 2 ) )
+            break;
+
+        //try to fill the horizontal space
+        if( $fontSize >= $maxFont && $textProperties[ 'textWidth' ] >= $minTextDesiredWidth )
             break;
     }
 
@@ -169,7 +234,8 @@ function getFontSize( Imagick $image, ImagickDraw $draw, $caption, $rows = 1 )
 
     return array(
         $fontSize,
-        $caption
+        $caption,
+        $textProperties
     );
 }
 
